ff698f7cac1c12bcfb121d943af60ee56e431957
Julian A Rivera C <48961526+JulianAndresRiveraCarrillo@users.noreply.github.com>
2022-03-08 15:56:18 -0500

    first commit

---
+++ src/main/java/com/Taller1_RiveraJulian/application/Taller1RiveraJulianApplication.java
@@ -0,0 +1,13 @@
+package com.Taller1_RiveraJulian.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1RiveraJulianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1RiveraJulianApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Taller1_RiveraJulian/repository/ProductModelRepository.java
@@ -0,0 +1,9 @@
+package com.Taller1_RiveraJulian.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Taller1_RiveraJulian.model.prod.Productmodel;
+
+public interface ProductModelRepository extends CrudRepository<Productmodel, Integer>{
+
+}
